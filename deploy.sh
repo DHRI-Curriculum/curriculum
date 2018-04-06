afa7c41191ec80300801a3eb9475ca0d9016fbe6
Patrick Smyth <patricksmyth01@gmail.com>
2018-04-06 19:04:24 -0400

    Fixed links Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,19 @@
+#!/bin/bash
+
+set -e
+
+gitbook install && gitbook build
+
+echo "Removing old docs..."
 rm -rf docs
+
+echo "Recreating docs folder..."
 cp -r _book docs
-git add -A
+
+echo "Committing changes..."
+git add docs
 git commit -m "Automated push"
+
 git push origin master
 
+exit 0
